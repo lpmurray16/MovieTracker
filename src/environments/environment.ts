df4ffa4d310eb9e8eff1d2736de56bc49cd8e42c
Logan Murray <lpmurray16@users.noreply.github.com>
2025-03-05 16:48:29 -0600

    asdasd

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,14 +3,14 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: '___FIREBASE_API_KEY___',
-    authDomain: '___FIREBASE_AUTH_DOMAIN___',
-    projectId: '___FIREBASE_PROJECT_ID___',
-    storageBucket: '___FIREBASE_STORAGE_BUCKET___',
-    messagingSenderId: '___FIREBASE_MESSAGING_SENDER_ID___',
-    appId: '___FIREBASE_APP_ID___',
+    apiKey: 'FIREBASE_API_KEY',
+    authDomain: 'FIREBASE_AUTH_DOMAIN',
+    projectId: 'FIREBASE_PROJECT_ID',
+    storageBucket: 'FIREBASE_STORAGE_BUCKET',
+    messagingSenderId: 'FIREBASE_MESSAGING_SENDER_ID',
+    appId: 'FIREBASE_APP_ID',
   },
   tmdb: {
-    apiKey: '___TMDB_API_KEY___',
+    apiKey: 'TMDB_API_KEY',
   },
 };
